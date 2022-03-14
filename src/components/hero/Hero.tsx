26b14a8a18c187ae10057b830990d20acd6d7eaf
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-14 15:19:16 +0000

    Folder Restructure and Initial Design

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <div className="hero" id="home">
+      <div className="hero__content">
+        <h1 className="gradient__text">Hi, I'm Jake</h1>
+        <p>Computer Science Graduate</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__links">
+        <div className="navbar__links_logo">
+          <img></img>
+        </div>
+        <nav className="navbar__links_container">
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/projects">Projects</a>
+          <a href="/skills">Skills</a>
+          <a href="/contact">Get in Contact</a>
+        </nav>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
