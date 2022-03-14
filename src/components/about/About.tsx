0d36c82ea0a2702f0516bbf27919889e699473e3
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-14 16:21:41 +0000

    Initial Structure

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,7 +3,41 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <div className="about__img-container"></div>
+      <div className="about__bio"></div>
+      <div className="about__sections">
+        <button>Skills</button>
+        <button>Education</button>
+        <button>Hobbies</button>
+      </div>
+
+      <div className="about__skills">
+        <p>Front-End</p>
+        <ul className="about__skills-frontend horizontal-list">
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>SASS</li>
+          <li>JS</li>
+          <li>TS</li>
+          <li>React</li>
+        </ul>
+        <p>Other</p>
+        <ul className="about__skills-other horizontal-list">
+          <li>Python</li>
+          <li>Sqlite3</li>
+          <li>Qt (Python)</li>
+        </ul>
+      </div>
+      <div className="about__education"></div>
+      <div className="about__hobbies">
+        <ul>
+          <li>Vehicle Mechanics</li>
+          <li>Engine Tuning</li>
+          <li>DIY</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
