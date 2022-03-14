524fbdacf40d4a0dc7be8e03d2f2314d448af623
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-14 16:22:40 +0000

    Implement span to title

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="hero" id="home">
       <div className="hero__content">
-        <h1 className="gradient__text">Hi, I'm Jake</h1>
+        <h1 className="gradient__text">Hi, I'm <span>Jake</span></h1>
         <p>Computer Science Graduate</p>
       </div>
       
