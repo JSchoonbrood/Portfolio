0da8c49d7887c937358b0cb616af267efe78028e
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-14 17:47:25 +0000

    Imported hero img

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './hero.css'
+import { default as hero_img } from '../../images/hero_dev.svg'
 
 const Hero = () => {
   return (
@@ -8,7 +9,10 @@ const Hero = () => {
         <h1 className="gradient__text">Hi, I'm <span>Jake</span></h1>
         <p>Computer Science Graduate</p>
       </div>
-      
+
+      <div className="hero__image">
+          <img src={hero_img} />
+        </div>
     </div>
   )
 }
