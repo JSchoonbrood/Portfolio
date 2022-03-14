dba85016232a538f3bdff2d50effda772eb644dd
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-14 20:44:46 +0000

    Replaced main div id

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import { default as hero_img } from '../../images/hero_dev.svg'
 
 const Hero = () => {
   return (
-    <div className="hero" id="home">
+    <div className="hero" id="hero">
       <div className="hero__content">
         <h1 className="gradient__text">Hi, I'm <span>Jake</span></h1>
         <p>Computer Science Graduate</p>
