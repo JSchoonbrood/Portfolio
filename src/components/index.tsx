24937caab271de6015c1b1f8f81688d260ce9bbd
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-17 15:25:14 +0000

    Implemented new sidebar

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as About } from './about/About';
 export { default as Hero } from './hero/Hero';
 export { default as Navbar } from './navbar/Navbar';
 export { default as Projects } from './projects/Projects';
+export { default as Left } from './sidebar/Left';
