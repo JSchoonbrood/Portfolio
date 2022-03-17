162aa5897ced847548523ebe5fdc9d1fbc4926a1
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-17 15:28:31 +0000

    Implemented new button and fixed JS hyperlink

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,12 +7,13 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__links">
-        <a><h1>JS</h1></a>
+        <a href="#app"><h1>JS</h1></a>
         <nav className="navbar__links_container">
           <a href="#app"><AiOutlineHome /> Home</a>
           <a href="#about"><AiOutlineUser /> About</a>
           <a href="#projects"><MdOutlineMonitor /> Projects</a>
           <a href="#contact"><AiOutlineContacts /> Contact</a>
+          <button>Resume</button>
         </nav>
       </div>
     </div>
