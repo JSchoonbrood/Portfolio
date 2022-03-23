b796ad5c2fbd7cff4e96512ab4b9b3f49d1f5fad
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-23 16:21:46 +0000

    New functional components

---
+++ src/components/about/hobbies/Hobbies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hobbies = () => {
+	return (
+		<div className="about__hobbies">
+			<ul>
+				<li>Vehicle Mechanics</li>
+				<li>Engine Tuning</li>
+				<li>DIY</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Hobbies;
