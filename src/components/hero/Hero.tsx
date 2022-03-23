76082082427ced3427536b45ff30d0cbff2822a7
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-23 16:22:07 +0000

    Reformat

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import './hero.scss'
-import { default as hero_img } from '../../images/hero_dev.svg'
-import Desc from "./type"
+import React from "react";
+import "./hero.scss";
+import { default as hero_img } from "../../images/hero_dev.svg";
+import Desc from "./type";
 
 const Hero = () => {
-  return (
-    <div className="hero" id="hero">
-      <div className="hero__content">
-        <h1>Hi! <span className="waving-hand">👋🏻</span><br/>I'm <span>Jake!</span></h1>
-        <Desc />
-      </div>
+	return (
+		<div className="hero" id="hero">
+			<div className="hero__content">
+				<h1>
+					Hi! <span className="waving-hand">👋🏻</span>
+					<br />
+					I'm <span>Jake!</span>
+				</h1>
+				<Desc />
+			</div>
 
-      <div className="hero__image">
-          <img src={hero_img} alt="Clipart Developer"/>
-        </div>
-    </div>
-  )
-}
+			<div className="hero__image">
+				<img src={hero_img} alt="Clipart Developer" />
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
