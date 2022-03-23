188aeeee457428f49d8089377ac29a38221e1ba6
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-23 21:43:11 +0000

    Icons import file

---
+++ src/components/about/skills/icons.tsx
@@ -0,0 +1,23 @@
+import css3 from './../../../images/icons/css3.svg'
+import html5 from './../../../images/icons/html5.svg'
+import js from './../../../images/icons/js.svg'
+import python from './../../../images/icons/python.svg'
+import qt from './../../../images/icons/qt.svg'
+import react from './../../../images/icons/react.svg'
+import sass from './../../../images/icons/sass.svg'
+import sqlite from './../../../images/icons/sqlite.svg'
+import ts from './../../../images/icons/ts.svg'
+
+const icons = {
+  css3,
+  html5,
+  js,
+  python,
+  qt,
+  react,
+  sass,
+  sqlite,
+  ts
+}
+
+export default icons;
