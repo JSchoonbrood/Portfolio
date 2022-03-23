96edfacebcecdced6b9625e6b6bfe84255fd1739
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-23 16:40:00 +0000

    Reformat

--- src/components/sidebar/Left.tsx
+++ src/components/sidebar/Left.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import { VscGithubAlt } from 'react-icons/vsc'
-import { FiLinkedin } from 'react-icons/fi'
-import { AiOutlineMail } from 'react-icons/ai'
-import './left.scss'
+import React from "react";
+import { VscGithubAlt } from "react-icons/vsc";
+import { FiLinkedin } from "react-icons/fi";
+import { AiOutlineMail } from "react-icons/ai";
+import "./left.scss";
 
 const left = () => {
-  return (
-    <div className="sidebar">
-        <div className="sidebar__elements">
-            <ul>
-                <li><a href="https://github.com/JSchoonbrood" target="_blank" rel="noreferrer"><VscGithubAlt className="icon"/></a></li>
-                <li><a href="https://www.linkedin.com/in/JSchoonbrood/" target="_blank" rel="noreferrer"><FiLinkedin /></a></li>
-                <li><a href="mailto:schoonbrood.jake@gmail.com?subject=Hello!" target="_blank" rel="noreferrer"><AiOutlineMail /></a></li>
-            </ul>
-            <div className="sidebar-vl"></div>
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="sidebar">
+			<div className="sidebar__elements">
+				<ul>
+					<li>
+						<a
+							href="https://github.com/JSchoonbrood"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<VscGithubAlt className="icon" />
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.linkedin.com/in/JSchoonbrood/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FiLinkedin />
+						</a>
+					</li>
+					<li>
+						<a
+							href="mailto:schoonbrood.jake@gmail.com?subject=Hello!" // {navigator.clipboard.writeText(textToCopy)}}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<AiOutlineMail />
+						</a>
+					</li>
+				</ul>
+				<div className="sidebar-vl"></div>
+			</div>
+		</div>
+	);
+};
 
-export default left
+export default left;
