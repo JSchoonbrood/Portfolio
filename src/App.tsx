f78e7934c79d96ee28d29c365d4ffd43f046718d
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-24 17:29:16 +0000

    Format

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 import { Routes, Route, Link } from "react-router-dom";
-import { About, Hero, Navbar, Projects, Left } from './components';
-import { Footer } from './containers';
+import { About, Hero, Navbar, Projects, Left } from "./components";
+import { Footer } from "./containers";
 
-import './App.scss';
-
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App" id="app">
-      <div className="gradient__bg">
-        <div className="margin-left">
-          <Navbar />
-          <div className="main-screen">
-            <div className="sidebar">
-              <Left />
-            </div>
-            <div className="components">
-            <Routes>
-              <Route path="/" element={<Hero />} />
-              <Route path="about" element={<About />} />
-              <Route path="projects" element={<Projects />} />
-            </Routes>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App" id="app">
+			<div className="gradient__bg">
+				<div className="margin-left">
+					<Navbar />
+					<div className="main-screen">
+						<div className="sidebar">
+							<Left />
+						</div>
+						<div className="components">
+							<Routes>
+								<Route path="/" element={<Hero />} />
+								<Route path="about" element={<About />} />
+								<Route path="projects" element={<Projects />} />
+							</Routes>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
