28cec220ed9bb60a641505bc6f5961be70e1b9bb
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-29 16:27:20 +0100

    New index file for passing multiple images

---
+++ src/images/index.tsx
@@ -0,0 +1,7 @@
+import placeholder from "../images/placeholder.png";
+import pollution_prediction from "../images/pollution_prediction.png";
+
+export {
+    placeholder,
+    pollution_prediction
+}
