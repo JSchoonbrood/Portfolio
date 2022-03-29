eb97d0ded60e870ce25d974a4b1c5bc3962599f8
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-29 10:57:54 +0100

    Counter removal and new section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,41 +6,64 @@ import projects from "./ProjectData";
 import "./projects.scss";
 
 const Projects = () => {
-  let counter = 0;
-  return (
-    <div className="page">
-      <div className="featured-projects">
-        <ul className="container">
-          {projects
-            .filter(({ featured }) => featured)
-            .map(
-              ({ title, image, description, code, live, tools}) => {
-                counter += 1
-                return (
-                  <li>
-                    <div className={counter % 2 ? "project-even" : "project-odd"}>
-                      <h1 className="title">{title}</h1>
-                      <img src={image} className="image" alt="placeholder"></img>
-                      <p className="description">{description}</p>
-                      <div className="links">
-                        <a href={code}><VscGithubAlt /></a>
-                        <a href={live}><FaExternalLinkAlt /></a>
-                      </div>
-                      <ul className="icons">
-                        {tools.map((elem) => {
-                          return <li>{elem}</li>;
-                        })}
-                      </ul>
-                    </div>
-                  </li>
-                );
-              }
-            )}
-        </ul>
-      </div>
-      <div className="other-projects"></div>
-    </div>
-  );
+	return (
+		<div className="page">
+			<div className="featured-projects">
+				<ul className="container">
+					{projects
+						.filter(({ featured }) => featured)
+						.map(({ title, image, description, code, live, tools }, index) => {
+							return (
+								<li>
+									<div className={index % 2 ? "project-left" : "project-right"}>
+										<h1 className="title">{title}</h1>
+										<img src={image} className="image" alt="placeholder"></img>
+										<p className="description">{description}</p>
+										<div className="links">
+											<a href={code}>
+												<VscGithubAlt />
+											</a>
+											<a href={live}>
+												<FaExternalLinkAlt />
+											</a>
+										</div>
+										<ul className="icons">
+											{tools.map((elem) => {
+												return <li>{elem}</li>;
+											})}
+										</ul>
+									</div>
+								</li>
+							);
+						})}
+				</ul>
+			</div>
+			<div className="other-projects">
+				<ul>
+					{projects
+						.filter(({ featured }) => !featured)
+						.map(({ title, description, live, tools }) => {
+							return (
+								<li>
+									<h1>{title}</h1>
+									<p>{description}</p>
+
+									<a href={live}>
+										<FaExternalLinkAlt />
+									</a>
+
+									<ul className="icons">
+										{tools.map((elem) => {
+											return <li>{elem}</li>;
+										})}
+									</ul>
+								</li>
+							);
+						})}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
