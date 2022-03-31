0ab84f2a204755eab405705aa8d85be2b6fd37ec
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-03-31 18:24:45 +0100

    ClassName overhaul

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,68 +1,75 @@
 import React from "react";
-// import { FaExternalLinkAlt } from "react-icons/fa";
-import { VscGithubAlt } from "react-icons/vsc";
-// import { IoDocumentText } from "react-icons/io5";
 import projects from "./ProjectData";
 import "./projects.scss";
 
 const Projects = () => {
-	return (
-		<div className="page">
-			<div className="featured-projects">
-				<ul className="container">
-					{projects
-						.filter(({ featured }) => featured)
-						.map(({ title, image, description, code, live, tools }, index) => {
-							return (
-								<li>
-									<div className={index % 2 ? "project-left" : "project-right"}>
-										<h1 className="title">{title}</h1>
-										{image}
-										<p className="description">{description}</p>
-										<div className="links">
-											<a href={code}>
-												<VscGithubAlt />
-											</a>
-											{live}
-										</div>
-										<ul className="icons">
-											{tools.map((elem) => {
-												return <li>{elem}</li>;
-											})}
-										</ul>
-									</div>
-								</li>
-							);
-						})}
-				</ul>
-			</div>
-			<div className="other-projects">
-        <h1 className="other-projects-title">Other Projects</h1>
-				<ul className="container">
-					{projects
-						.filter(({ featured }) => !featured)
-						.map(({ title, description, live, tools }) => {
-							return (
-								<li className="other-projects-items">
-									<h1>{title}</h1>
-									<p>{description}</p>
+  return (
+    <div className="page">
+      <div className="featured">
+        <ul className="featured__elements">
+          {projects
+            .filter(({ featured }) => featured)
+            .map(({ title, image, description, code, live, tools }, index) => {
+              return (
+                <li
+                  className={
+                    index % 2
+                      ? "featured__elements-items project-left"
+                      : "featured__elements-items project-right"
+                  }
+                >
+                  <h1 className="featured__elements-title">{title}</h1>
+                  <div className="featured__elements-image">
+                    {image}
+                  </div>
+                  <p className="featured__elements-desc">{description}</p>
+                  <div className="featured__elements-links">
+                    {code}
+                    {live}
+                  </div>
+                  <ul className="featured__elements-tools">
+                    {tools.map((elem) => {
+                      return (
+                        <li className="featured__elements-tools-item">
+                          {elem}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+      <div className="other">
+        <h1 className="other-title">Other Projects</h1>
+        <ul className="other__elements">
+          {projects
+            .filter(({ featured }) => !featured)
+            .map(({ title, description, live, code, tools }) => {
+              return (
+                <li className="other__elements-items">
+                  <h1 className="other__elements-title">{title}</h1>
+                  <ul className="other__elements-links">
+                    {code}
+                    {live}
+                  </ul>
+                  <p className="other__elements-desc">{description}</p>
 
-									{/* <a href={live}>
-										{live}<FaExternalLinkAlt />
-									</a> */}
-
-									<ul className="icons">
-										{tools.map((elem) => {
-											return <li>{elem}</li>;
-										})}
-									</ul>
-								</li>
-							);
-						})}
-				</ul>
-			</div>
-		</div>
-	);
+                  <ul className="other__elements-tools">
+                    {tools.map((elem) => {
+                      return (
+                        <li className="other__elements-tools-item">{elem}</li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
