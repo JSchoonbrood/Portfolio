60ccfc80225baa38396b290e0aacb6511a2f9344
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-07 23:35:42 +0100

    Navbar Mobile Layout implemented

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,13 +3,30 @@ import {
   AiOutlineUser,
   AiOutlineContacts,
 } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineMonitor } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import "./navbar.scss";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const navbarControl = () => {
+    if (expanded) {
+      setExpanded(false);
+    } else {
+      setExpanded(true);
+    }
+  };
+
   return (
     <div className="navbar navbar-scrolled">
+      <div className="navbar__expand">
+        <button className="menu" onClick={navbarControl}>
+          <GiHamburgerMenu size={24} />
+        </button>
+      </div>
       <div className="navbar__links">
         <button
           onClick={() => window.location.reload()}
@@ -17,31 +34,41 @@ const Navbar = () => {
         >
           <h1>JS</h1>
         </button>
-        <nav className="navbar__links_container">
+        <nav
+          className={
+            expanded
+              ? "active-nav navbar__links_container"
+              : "navbar__links_container"
+          }
+        >
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? "active" : undefined)}
+            className={({ isActive }) => (isActive ? "active link" : "link")}
+            onClick={navbarControl}
           >
             <AiOutlineHome className="icon" />
             Home
           </NavLink>
           <NavLink
             to="/about"
-            className={({ isActive }) => (isActive ? "active" : undefined)}
+            className={({ isActive }) => (isActive ? "active link" : "link")}
+            onClick={navbarControl}
           >
             <AiOutlineUser className="icon" />
             About
           </NavLink>
           <NavLink
             to="/projects"
-            className={({ isActive }) => (isActive ? "active" : undefined)}
+            className={({ isActive }) => (isActive ? "active link" : "link")}
+            onClick={navbarControl}
           >
             <MdOutlineMonitor className="icon" />
             Projects
           </NavLink>
           <NavLink
             to="/contact"
-            className={({ isActive }) => (isActive ? "active" : undefined)}
+            className={({ isActive }) => (isActive ? "active link" : "link")}
+            onClick={navbarControl}
           >
             <AiOutlineContacts className="icon" />
             Contact
