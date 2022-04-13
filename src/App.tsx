bc409b2df1a45888d372e2b55ce460608e83b47e
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-13 21:48:53 +0100

    Read from store to determine whether to apply css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Ref } from "react";
 import { Routes, Route } from "react-router-dom";
 import { About, Hero, Navbar, Projects, Left, Contact } from "./components";
+import { useAppSelector } from "./data/hooks";
 
 import "./App.scss";
 
 function App() {
+  const value = useAppSelector((state) => state.focus.value);
   return (
     <div className="App" id="app">
       <div className="layout gradient__bg">
@@ -15,7 +17,12 @@ function App() {
           <div className="layout__container-sidebar">
             <Left />
           </div>
-          <div className="layout__container-components">
+          <div 
+          className={
+            value
+              ? "layout__container-components blur"
+              : "layout__container-components"
+          }>
             <Routes>
               <Route path="/" element={<Hero />} />
               <Route path="about" element={<About />} />
