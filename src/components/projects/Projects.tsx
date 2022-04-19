00f9afb197c66d011038b83598486fb22b9b3669
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-19 22:09:41 +0100

    Tooltip integration and code optimisation

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -9,34 +9,53 @@ const Projects = () => {
         <ul className="featured__elements">
           {projects
             .filter(({ featured }) => featured)
-            .map(({ title, image, description, code, live, tools }, index) => {
-              return (
-                <li
-                  className={
-                    index % 2
-                      ? "featured__elements-items project-left"
-                      : "featured__elements-items project-right"
-                  }
-                >
-                  <h1 className="featured__elements-title">{title}</h1>
-                  <div className="featured__elements-image">{image}</div>
-                  <p className="featured__elements-desc">{description}</p>
-                  <div className="featured__elements-links">
-                    {code}
-                    {live}
-                  </div>
-                  <ul className="featured__elements-tools">
-                    {tools.map((elem) => {
-                      return (
-                        <li className="featured__elements-tools-item">
-                          {elem}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </li>
-              );
-            })}
+            .map(
+              (
+                { title, image, description, code, live, tools, status },
+                index
+              ) => {
+                return (
+                  <li
+                    className={
+                      "featured__elements-items" +
+                      (index % 2 ? " project-left" : " project-right")
+                    }
+                  >
+                    <div className="featured__elements-title">
+                      <h1 className="featured__elements-title-text">{title}</h1>
+                      <br />
+                      <div className="featured__elements-title-status">
+                        {status ? (
+                          <span className="dot-green tooltip">
+                            <span className="tooltiptext">Project Completed</span>
+                          </span>
+                        ) : (
+                          <span className="dot-red tooltip">
+                            <span className="tooltiptext">Project Under Development</span>
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <span className="featured__elements-status"></span>
+                    <div className="featured__elements-image">{image}</div>
+                    <p className="featured__elements-desc">{description}</p>
+                    <div className="featured__elements-links">
+                      {code}
+                      {live}
+                    </div>
+                    <ul className="featured__elements-tools">
+                      {tools.map((elem) => {
+                        return (
+                          <li className="featured__elements-tools-item">
+                            {elem}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </li>
+                );
+              }
+            )}
         </ul>
       </div>
       <div className="other">
