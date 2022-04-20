7e6333f4da12bdf450a6b1d1714e27e5cde73d0c
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-20 16:05:04 +0100

    Mockup table data

---
+++ src/components/archive/components/TableData.tsx
@@ -0,0 +1,6 @@
+const data = [
+  {id: 1, state: "folder", name: "MaxxECU", lastModified:"Today"},
+  {id: 2, state: "folder", name: "Calculators", lastModified:"Today"},
+];
+
+export default data;
