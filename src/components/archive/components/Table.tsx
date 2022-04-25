e53c0a5c181b6140bba569344cf24244bc7f9717
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-25 13:53:30 +0100

    Dynamically rendering table with subrows

--- src/components/archive/components/Table.tsx
+++ src/components/archive/components/Table.tsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useMemo } from "react";
+import { useTable, useExpanded } from "react-table";
+import TableData, { COLUMNS } from "./TableData";
 
 const Table = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => TableData, []);
+  const initialState = { hiddenColumns: ['id'] };
+
+  // @ts-ignore
+  const tableInstance = useTable({ columns, data, initialState }, useExpanded);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state: { expanded },
+  } = tableInstance;
+
   return (
     <div>
-      <table>
+      <table {...getTableProps()}>
         <thead>
-          <tr>
-            <th></th>
-          </tr>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-          </tr>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
