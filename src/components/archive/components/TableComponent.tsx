bb2702f3710736ff6d49872f1eadcfd2160210a9
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-25 15:23:38 +0100

    Filename change for clarity

--- src/components/archive/components/TableComponent.tsx
+++ src/components/archive/components/TableComponent.tsx
@@ -21,21 +21,21 @@ const Table = () => {
 
   return (
     <div>
-      <table {...getTableProps()}>
-        <thead>
+      <table {...getTableProps()} className="table">
+        {/* <thead className="table__header">
           {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
+            <tr {...headerGroup.getHeaderGroupProps()} className="table__header-row">
               {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
           ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
+        </thead> */}
+        <tbody {...getTableBodyProps()} className="table__body">
           {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr {...row.getRowProps()}>
+              <tr {...row.getRowProps()} className={"table__body-row" + (row.depth === 1 ? " expanded" : "")}>
                 {row.cells.map((cell) => {
                   return (
                     <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
