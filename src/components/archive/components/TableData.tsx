5aa742553d251c53639f2094a2387a5bf53e4b4a
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-04-25 13:54:37 +0100

    Initial table data implementation

--- src/components/archive/components/TableData.tsx
+++ src/components/archive/components/TableData.tsx
@@ -1,6 +1,61 @@
-const data = [
-  {id: 1, state: "folder", name: "MaxxECU", lastModified:"Today"},
-  {id: 2, state: "folder", name: "Calculators", lastModified:"Today"},
+import { BiRightArrow, BiDownArrow } from "react-icons/bi";
+import { NavLink } from "react-router-dom";
+
+const TableData = [
+  {
+    id: "1",
+    name: "MaxxECU",
+    lastModified: "Today",
+    subRows: [
+      {name: <NavLink to="immosetup">Immobiliser Setup</NavLink>},
+      {name: "Clutch Slip Logging"}
+    ],
+  },
+  {
+    id: "2",
+    name: "Calculators",
+    lastModified: "Today",
+    subRows: [
+      {name: "Wheel Diamater Calculator"},
+      {name: "Gear Ratio Calculator"}
+    ],
+  },
 ];
 
-export default data;
+export const COLUMNS = [
+  {
+    id: "expander",
+    // @ts-ignore
+    Header: ({getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
+      <span {...getToggleAllRowsExpandedProps()}>
+        {isAllRowsExpanded ? <BiDownArrow /> : <BiRightArrow />}
+      </span>
+    ),
+    // @ts-ignore
+    Cell: ({row}) =>
+    row.canExpand ? (
+      <span
+        {...row.getToggleRowExpandedProps({
+          style: {
+            // expanded styles
+          },
+        })}>
+        {row.isExpanded ? <BiDownArrow /> : <BiRightArrow />}
+      </span>
+    ) : null,
+  },
+  {
+    Header: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Last Modified",
+    accessor: "lastModified",
+  },
+];
+
+export default TableData;
