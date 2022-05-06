54e073aca0b438dd12a6699737075885a1f6df37
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-05-07 00:00:10 +0100

    Initial education data

---
+++ src/components/about/education/EducationData.tsx
@@ -0,0 +1,28 @@
+const EducationData = [
+  {
+    name: "University of York",
+    qualification: "Bachelors of Engineering",
+    grade: "First Class Honours (76%)",
+    years: "2018-2021",
+    desc: "",
+    notableAchievements: [
+      "Artificial Intelligence (96%)",
+      "Systems (89%)",
+      "Final Year Project (79%)",
+      "Workshop Assistant for York AI Society",
+    ]
+  },
+  {
+    name: "Queensbury Academy",
+    qualification: "A Levels",
+    grade: null,
+    years: "2016-2018",
+    desc: "ABB - Computer Science, Mathematics, Physics",
+    notableAchievements: [
+      "Self-taught Computer Science for two years due to shortage of faculty",
+      "Travelled between two schools to study Maths & Physics"
+    ]
+  },
+];
+
+export default EducationData;
