05003b82ddd30f311fe2a37730146d97ee6505d0
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-05-07 00:00:00 +0100

    Dynamic rendering data

--- src/components/about/education/Education.tsx
+++ src/components/about/education/Education.tsx
@@ -1,23 +1,29 @@
 import React from "react";
+import EducationData from "./EducationData";
 
 const Education = () => {
   return (
     <div className="about__education">
-      <div className="about__education-block">
-        <div className="about__education-block-content">
-          <h3>2019-2021</h3>
-          <h4>University Of York</h4>
-          <p>Graduated with First Class Honours in Computer Science (BEng). </p>
-        </div>
-      </div>
-
-      <div className="about__education-block">
-        <div className="about__education-block-content">
-          <h3>2017-2019</h3>
-          <h4>Queensbury Academy - ALevels</h4>
-          <p></p>
-        </div>
-      </div>
+      <ul>
+        {EducationData.map(({ name, qualification, grade, years, desc, notableAchievements }) => {
+          return (
+            <li className="about__education-block">
+              <h1 className="about__education-block-title">{name}</h1>
+              <h1 className="about__education-block-years">{years}</h1> 
+              <h2 className="about__education-block-qualification">{qualification}</h2>
+              <h2 className="about__education-block-grade">{grade}</h2>
+              <p className="about__education-block-desc">{desc}</p>
+              <ul className="about__education-block-list">
+                {notableAchievements.map((value) => {
+                  return (
+                    <li>{value}</li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 };
