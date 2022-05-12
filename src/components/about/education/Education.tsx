0d37747e29b114f9dfc3ef1166d43dfb27a975c5
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-05-12 21:50:47 +0100

    Updated dynamic rendering

--- src/components/about/education/Education.tsx
+++ src/components/about/education/Education.tsx
@@ -3,23 +3,29 @@ import EducationData from "./EducationData";
 
 const Education = () => {
   return (
-    <div className="about__education">
+    <div className="education">
       <ul>
-        {EducationData.map(({ name, qualification, grade, years, desc, notableAchievements }) => {
+        {EducationData.map(({ name, qualification, grade, years, desc, notableAchievements, image }) => {
           return (
-            <li className="about__education-block">
-              <h1 className="about__education-block-title">{name}</h1>
-              <h1 className="about__education-block-years">{years}</h1> 
-              <h2 className="about__education-block-qualification">{qualification}</h2>
-              <h2 className="about__education-block-grade">{grade}</h2>
-              <p className="about__education-block-desc">{desc}</p>
-              <ul className="about__education-block-list">
-                {notableAchievements.map((value) => {
-                  return (
-                    <li>{value}</li>
-                  )
-                })}
-              </ul>
+            <li className="education-block">
+              <div className="education-block-imagecontainer">
+                {image}
+              </div>
+              <div className="education-block-con">
+                <h1 className="education-block-title">{name}</h1>
+                <h1 className="education-block-qualification">{qualification}</h1>
+                {/* <h1 className="education-block-years">{years}</h1>  */}
+                <h2 className="education-block-grade">{grade}</h2>
+                <p className="education-block-desc">{desc}</p>
+                <ul className="education-block-list">
+                  {notableAchievements.map((value) => {
+                    return (
+                      <li>{value}</li>
+                    )
+                  })}
+                </ul>
+              </div>
+              
             </li>
           )
         })}
