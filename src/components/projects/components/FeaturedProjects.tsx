ea505f5f4b23910761e25937997ef514802538f2
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-06-06 15:13:16 +0100

    Code overhaul

--- src/components/projects/components/FeaturedProjects.tsx
+++ src/components/projects/components/FeaturedProjects.tsx
@@ -4,59 +4,47 @@ import projects from "./ProjectData";
 const FeaturedProjects = () => {
   return (
     <div className="featured">
-        <h1 className="featured-title">Featured Projects</h1>
-        <ul className="featured__elements">
-          {projects
-            .filter(({ featured }) => featured)
-            .map(
-              (
-                { title, image, description, code, live, tools, status },
-                index
-              ) => {
-                return (
-                  <li
-                    className={
-                      "featured__elements-items" +
-                      (index % 2 ? " project-left" : " project-right")
-                    }
-                  >
-                    <div className="featured__elements-title">
-                      <h1 className="featured__elements-title-text">{title}</h1>
-                      <div className="featured__elements-title-status">
-                        {status ? (
-                          <span className="dot-green tooltip">
-                            <span className="tooltiptext">Project Completed</span>
-                          </span>
-                        ) : (
-                          <span className="dot-red tooltip">
-                            <span className="tooltiptext">Project Under Development</span>
-                          </span>
-                        )}
+      <h1 className="featured-title">Featured Projects</h1>
+      <ul className="featured__elements">
+        {projects
+          .filter(({ featured }) => featured)
+          .map(
+            (
+              { title, image, description, code, live, tools, status },
+              index
+            ) => {
+              return (
+                <li
+                  className={
+                    "featured__elements-items" +
+                    (index % 2 ? " project-left" : " project-right")
+                  }
+                >
+                  <div className="flex-column">
+                      <h1 className="featured__elements-title">{title}</h1>
+                      <p className="featured__elements-desc">{description}</p>
+                      <ul className="featured__elements-tools">
+                        {tools.map((elem) => {
+                          return (
+                            <li className="featured__elements-tools-item">
+                              {elem}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <div className="featured__elements-links">
+                        {code}
+                        {live}
                       </div>
-                    </div>
-                    <span className="featured__elements-status"></span>
-                    <span className="featured__elements-image-filter"></span>
-                    <div className="featured__elements-image">{image}</div>
-                    <p className="featured__elements-desc">{description}</p>
-                    <div className="featured__elements-links">
-                      {code}
-                      {live}
-                    </div>
-                    <ul className="featured__elements-tools">
-                      {tools.map((elem) => {
-                        return (
-                          <li className="featured__elements-tools-item">
-                            {elem}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </li>
-                );
-              }
-            )}
-        </ul>
-      </div>
+                  </div>
+                  <span className="featured__elements-image-filter"></span>
+                  <div className="featured__elements-image">{image}</div>
+                </li>
+              );
+            }
+          )}
+      </ul>
+    </div>
   )
 }
 
