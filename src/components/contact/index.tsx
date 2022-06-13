1e68aea0557e0d2d2a1dc693b9aff6b2aefbaf0e
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-06-13 11:17:03 +0100

    Removed temporary code

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -5,9 +5,6 @@ import "./contact.scss";
 const Contact = () => {
 	return ( 
     <div className="page">
-      <div className="title">
-        <h1>Contact Me</h1>
-      </div>
       <Form />
     </div>
   );
