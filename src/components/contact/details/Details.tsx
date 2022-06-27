8c82d29daf5baaeb0a34ec9e1e8d01a5bb38ebb6
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-06-27 18:21:44 +0100

    Copy to clipboard email

--- src/components/contact/details/Details.tsx
+++ src/components/contact/details/Details.tsx
@@ -2,13 +2,32 @@ import React from "react";
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Details = () => {
+  const [copied, setCopied] = React.useState(false);
+
+  const handleEmail = () => {
+    if (!copied) {
+      setCopied(true);
+      navigator.clipboard.writeText("schoonbrood.jake@gmail.com");
+
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  };
+
   return (
     <div className="cont-details">
       <ul className="cont-details__list">
         <li className="cont-details__item">
           <FaEnvelope className="cont-details__icon" />
           <h1 className="cont-details__title">Email</h1>
-          <p className="cont-details__desc" onClick={() => {navigator.clipboard.writeText("schoonbrood.jake@gmail.com")}}>schoonbrood.jake@gmail.com</p>
+          <p
+            className={"cont-details__desc tooltip" + (copied ? " tooltip-visible" : "")}
+            onClick={handleEmail}
+          >
+            schoonbrood.jake@gmail.com
+            <span className="tooltiptext">Copied</span>
+          </p>
         </li>
         <li className="cont-details__item">
           <FaGithub className="cont-details__icon" />
