7287c73b64d2c11437077cca701782163f6c1b78
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-07-19 14:18:23 +0100

    Hero links

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./hero.scss";
 import { default as hero_img } from "../../images/hero_dev.svg";
 import Desc from "./type";
-import { NavLink } from "react-router-dom";
+import {
+  Link,
+} from "react-scroll";
 
 const Hero = () => {
   return (
@@ -17,17 +19,37 @@ const Hero = () => {
           I'm an enthusiastic software engineer who enjoys solving problems.
         </p>
         <div className="hero__content-buttons">
-          <NavLink to="/projects" className="hero__content-buttons-project">Projects</NavLink>
-          <a href="https://github.com/JSchoonbrood" className="hero__content-buttons-github" target="_blank" rel="noreferrer">GitHub</a>
+          <Link                                                                                                                                                                                                                                    
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            className="hero__content-buttons-project"
+          >
+            Projects
+          </Link>
+          <a
+            href="https://github.com/JSchoonbrood"
+            className="hero__content-buttons-github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
         </div>
       </div>
 
-      <div className="hero__image">
+      <div className="hero__image" id="projects">
         <img src={hero_img} alt="Clipart Developer" />
       </div>
+
+      {/* <div className="background-skew">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none">
+            <polygon className="fill-white" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div> */}
     </div>
   );
 };
 
 export default Hero;
-
