08f66081d2daf66a86e8e007fe9ea38613c172ee
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-07-19 14:20:06 +0100

    id update

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import AdditionalProjects from "./components/AdditionalProjects";
 import FeaturedProjects from "./components/FeaturedProjects";
 import "./projects.scss";
-import { NavLink } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <div className="projects">
+    <div className="projects" id="projects">
       <FeaturedProjects />
       <AdditionalProjects />
     </div>
