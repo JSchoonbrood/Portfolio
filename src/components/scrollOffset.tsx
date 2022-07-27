d49f85b4c3f3f45e8a57cf5c05cdb1a6a5a74013
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-07-27 21:44:55 +0100

    Relocation for use in multiple components

---
+++ src/components/scrollOffset.tsx
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+const scrollOffset = (elem: HTMLElement) => {
+  const yCoordinate: number =
+    elem.getBoundingClientRect().top + window.pageYOffset;
+  const yOffset: number = -60;
+  $("html").animate({ scrollTop: yCoordinate + yOffset }, 500); // Scroll delay of 500ms
+};
+
+export default scrollOffset;
