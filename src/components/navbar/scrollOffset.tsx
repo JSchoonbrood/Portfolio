c65738778cc3502bd177b3e79665118eee43b689
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-07-27 21:46:07 +0100

    File moved

--- src/components/navbar/scrollOffset.tsx
+++
@@ -1,7 +0,0 @@
-const scrollOffset = (elem: HTMLElement) => {
-    const yCoordinate: number = elem.getBoundingClientRect().top + window.pageYOffset;
-    const yOffset: number = -60; 
-    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });  
-}
-
-export default scrollOffset
